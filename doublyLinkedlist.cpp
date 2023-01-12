36d81cbd9b98e43efee0e7294d37810ba0f55c5d
kelvin <kelvin950>
2023-01-12 15:39:45 +0000

    reversing  a doubly linked list

--- doublyLinkedlist.cpp
+++ doublyLinkedlist.cpp
@@ -147,7 +147,7 @@ void Reverse(struct Node *p){
         p->next =  p->prev ;
         p->prev =temp;
         p=p->prev;
-        if(p->next == NULL){
+        if(p!=NULL && p->next == NULL){
             first = p;
         }
     }
