99a874520110be68edb80d0473aa07528838b09b
kelvin <kelvin950>
2023-01-12 16:23:12 +0000

    creating the methods  linked list to a calss

--- ClassLinkedList.cpp
+++ ClassLinkedList.cpp
@@ -55,15 +55,17 @@ class  LinkedList{
             
             else{
                 Node *p = first;
+                
         for(int i= 0 ; i<pos-1 ; i++){
 
-            p= p->next ;  
+          p= p->next ;  
                 
         }       
-        temp->next =  p->next ;
-             p->next =  temp ;
+        temp->next =p->next ;
+           p->next =  temp ;
+          
                size++;
-            
+                 
             }
    } 
 
@@ -73,6 +75,50 @@ class  LinkedList{
     return  this->size;
    }
 
+ 
+  void Create(int A[] , int  n){
+
+    Node *temp =  new Node();
+    temp->data  =A[0] ;
+       first = temp ;
+       last =temp ; 
+       size++;
+      
+      for(int i= 1 ; i<n ;i++){
+            Node *temp =  new Node();
+    temp->data  =A[i] ;
+      last->next = temp;
+          last=temp;
+          last->next = NULL;
+          size++;
+       }
+
+  }
+void display(){
+ 
+ Node *p =  first;
+
+    while(p){
+        cout<<p->data<<endl;
+        p=p->next ;
+    }
+}
 } ;
 
 
+
+int main(){
+
+
+
+    LinkedList *list =  new LinkedList();
+       cout<<list->Length()<<endl;
+    int A[] =  {1,2,3,4};
+      
+      list->Create(A , 4);
+         
+        list->display();
+    list->Insert(4 , 5);
+    list->display();
+        cout<<list->Length()<<endl;
+}
