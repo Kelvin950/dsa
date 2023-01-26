a70496f961b519cf9c61b96471106f30f8f2908d
kelvin <kelvin950>
2023-01-26 14:03:44 +0000

    creating an add function to add two sparse matrices

--- sparseMatrixLinkedList.cpp
+++ sparseMatrixLinkedList.cpp
@@ -12,15 +12,134 @@ class Node{
 }; 
 
 
+Node *Add(Node *A , Node *B ,int n){
+
+          int  i = 0 ; 
+          int j = 0 ;
+   Node *C =  new Node[n];
+
+   while(i <n && j<n ){
+      
+      Node  *p = A[i].next ; 
+      Node *q = B[i].next; 
+      cout<<q->value<<p->value<<endl;
+      Node *first=NULL ,*last=NULL;
+      
+
+         while (p || q)
+         {
+            /* code */
+         if(p && q){
 
 
-int main(){
+               if(p->column < q->column){
+                  Node *temp = new Node();
+                  temp->column  =p->column ;
+                  temp->value = p->value ; 
+                  if(first==NULL){
+                    first=temp ;
+                    last=first;
+                    cout<<first->value<<endl;
+                      
+                  } 
+                  else{
+                     last->next=temp;
+                     last=temp;
+                     cout<<last->value<<endl;
+                  }
+                 p=p->next ;
+               }
+               else if(p->column > q->column){
+
+                  Node *temp = new Node();
+                  temp->column  =q->column ;
+                  temp->value = q->value ; 
+                  if(first==NULL){
+                    first=temp ;
+                    last=first;
+                    cout<<first->value<<endl;
+                      
+                  } 
+                  else{
+                     last->next=temp;
+                     last=temp;
+                     cout<<last->value<<endl;
+                  }
+                q=q->next ;
+               }
+
+            else{
+
+                Node *temp = new Node();
+                  temp->column  =q->column ;
+                  temp->value = q->value + p->value ; 
+                  if(first==NULL){
+                    first=temp ;
+                    last=first;
+                    cout<<first->value<<endl;
+                      
+                  } 
+                  else{
+                     last->next=temp;
+                     last=temp;
+                     cout<<last->value<<endl;
+                  }
+                q=q->next ;
+                p=p->next;
+               }
+            }
+         else if(p && !q){
+              
+              Node *temp = new Node();
+                  temp->column  =p->column ;
+                  temp->value = p->value ; 
+                  if(first==NULL){
+                    first=temp ;
+                    last=first;
+                    cout<<first->value<<endl;
+                      
+                  } 
+                  else{
+                     last->next=temp;
+                     last=temp;
+                     cout<<last->value<<endl;
+                  }
+                 p=p->next ;
+             
+         }
+      else if(!p && q){
+         Node *temp = new Node();
+                  temp->column  =q->column ;
+                  temp->value = q->value ; 
+                  if(first==NULL){
+                    first=temp ;
+                    last=first;
+                    cout<<first->value<<endl;
+                      
+                  } 
+                  else{
+                     last->next=temp;
+                     last=temp;
+                  }
+                 q=q->next ;
+      }
+
+         }
+         cout<<first->value;
+             C[i].next = first;
+         i++;
+         j++;
+         }
+         
+
+         return C;
+
+   } 
 
 
-  Node *A = new Node[2];
- 
 
-for(int i = 0 ; i < 2; i++){
+void Create(Node *q, int row){
+for(int i = 0 ; i < row; i++){
     cout<<"Enter the number of non-zero values in the row"<<endl;
     int column ;
     cin >>column ;
@@ -45,20 +164,50 @@ for(int i = 0 ; i < 2; i++){
 
 }
     
- A[i].next =  first ;
+ q[i].next =  first ;
 } 
+
+
+}
+
+
+void Display(Node *q , int row,int col){
+
  
- 
- for(int i = 0 ; i<2 ;i++){
-     Node *p = A[i].next;
-    for(int j =0 ;j<2 ;j++){
+ for(int i = 0 ; i<row ;i++){
+     Node *p = q[i].next;
+    for(int j =0 ;j< col;j++){
         if(j==p->column){
          cout<<p->value ; 
 
          if(p->next){
             p=p->next;
          }
-    }}
+    }
+    else{
+      cout<<"0";
+    }
+    }
+    cout<<endl;
  }
 
+}
+int main(){
+
+
+  Node *A = new Node[5];
+  Node *B =  new Node[5];
+ 
+Create(A , 5);
+Display(A , 5,6);
+ 
+
+Create(B ,5);
+cout<<endl;
+Display(B , 5, 6);
+ 
+
+ Node *C =  Add(A ,B , 5);
+cout<<endl;
+ Display(C, 5,6);
 }
