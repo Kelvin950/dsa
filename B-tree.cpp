1eb8cf1680636e70400b49a491c35906d1bd3a45
kelvin <kelvin950>
2023-03-01 23:30:15 +0000

    creating a class for a btree

---
+++ B-tree.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+
+using namespace std ; 
+
+
+class Nodequeue {
+ 
+ public:
+Node *data = NULL ; 
+ Nodequeue *next; 
+
+};
+
+class Queue{
+ 
+public:
+ Queue(){
+     
+     front= NULL;
+     rear= NULL;
+
+ }
+
+
+ bool isEmpty(){
+
+   return  front ==NULL;
+ }
+ 
+ 
+ void enqueue(Node  *x){
+      
+      Nodequeue *temp = new Nodequeue();
+      
+      if(temp ==NULL){
+        return;
+      }
+    
+    temp->data =x ;
+
+    if(isEmpty()){
+        front =rear= temp ;
+    }else{
+      
+      rear->next =  temp ; 
+      rear=temp;
+
+    }
+     
+ }
+ 
+
+ Node * dequeue(){
+
+    Node *x = NULL ; 
+
+    if(isEmpty()){
+        return  NULL;
+    }
+
+    Nodequeue *q = front ; 
+    front =front->next ;
+
+    x = q->data ;
+     delete q ;
+
+return x ; 
+ }
+
+ void display(){
+
+    Nodequeue  *q = front ; 
+     
+     while (q)
+     {
+        /* code */
+        cout<<q->data->data<<endl;
+        q=q->next ;
+     }
+     
+ }
+private:
+Nodequeue *front ; 
+Nodequeue *rear ; 
+} ;
+
+ 
+ class Node{
+
+    public:
+    Node *leftChilld; 
+    Node *rightChild;
+    int data;
+ }
+  ;
+class BTree{
+         
+         public:
+
+         BTree(){
+            root = NULL;
+         }
+    
+    void Preorder(Node *p) ;
+    void Postorder(Node *p);
+    void Inorder(Node *p);
+    void CreateTree(){
+
+     Node *p=NULL  ; Node *t=NULL;
+    Queue *queue= new Queue();
+      int data ; 
+ cout<<"Enter the data of the root"<<endl;
+ cin>> data ; 
+
+    root->data=data ;
+     queue->enqueue(root);  
+      
+
+      while(!queue->isEmpty())
+{
+
+
+  p = queue->dequeue(); 
+int x ; 
+   cout<<"Enter the value of left child, Enter -1 if null"<<endl; 
+cin >>x ;
+    
+  if(x!=-1){
+  t =  new Node(); 
+  t->data =x ; 
+  p->leftChilld =  t ;
+  queue->enqueue(t);
+  }  
+
+   cout<<"Enter the value of right child, Enter -1 if null"<<endl; 
+cin >>x ;
+    
+  if(x!=-1){
+  t =  new Node(); 
+  p->rightChild =  t ;
+  t->data =x ; 
+  queue->enqueue(t);
+  }  
+
+
+
+
+}  
+
+    };
+    void LevelOrder(Node *p);
+    void Height(Node *root);
+   void  CreateRoot(int data){
+        
+    root->data =  data ;
+     
+
+     }
+   
+ Node *Root(){
+
+    return root;
+ }
+
+
+private:
+    
+    Node *root ; 
+   
+
+} ;
