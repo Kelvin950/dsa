bfcad2542112cca28f625547f2424b47348ed4f1
kelvin <denlinato@gmail.com>
2023-07-28 16:50:39 +0000

    linear probing

--- hashing.js
+++ hashing.js
@@ -92,15 +92,61 @@ function insert(H, key){
     sort(H , index , key)
 
 }
+ 
+
+//linear probing
+
+function probe(H , index){
+ 
+
+    let i= 0
+    while(H[(index+i)%10] !=0){
+        i++
+    }
 
 
+    return (index+i)%10 ;
+}
 
-let  H =  Array(10).fill(null)
+function linearProbinginsert(H , key){
 
-insert(H , 12) ;
-insert(H, 22) 
-insert(H, 32)
+    let index =  hash(key) ;
+
+    if(H[index]!=0){
+         
+        index = probe(H , index);
+    }
+
+    H[index] = key
+
+}
 
 
+function linearProbingsearch(H,key){
+
+
+let index = hash(key) ;
+
+let i= 0 ;
+
+while(H[(index+i)%10] !== key){
+    i++ ;
+     if(H[(index+i)%10] == 0)return -1
+}
+
+return  (index+i)%10 ;
+}
+
+let  H =  Array(10).fill(0)
+
+// insert(H , 12) ;
+// insert(H, 22) 
+// insert(H, 32)
+
+linearProbinginsert(H , 55)
+linearProbinginsert(H , 5)
+linearProbinginsert(H , 100)
+linearProbinginsert(H, 10)
 console.log(H)
-console.log(search(H, 42));
+console.log(linearProbingsearch(H,6))
+// console.log(search(H, 42));
